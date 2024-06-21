206a178c989f52f30faa2131192cd43189d5d14c
Fabien Lamarche-Filion <fabien@marketops.com>
2024-06-21 08:56:52 -0400

    Initial commit

---
+++ lib/uid2_client_elixir.ex
@@ -0,0 +1,18 @@
+defmodule Uid2ClientElixir do
+  @moduledoc """
+  Documentation for `Uid2ClientElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Uid2ClientElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uid2_client_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule Uid2ClientElixirTest do
+  use ExUnit.Case
+  doctest Uid2ClientElixir
+
+  test "greets the world" do
+    assert Uid2ClientElixir.hello() == :world
+  end
+end
